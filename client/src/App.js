c15669ae5bb3354ba71bbc695a91691a989e479c
nadushsa <nadushsa@outlook.com>
2024-05-27 15:51:43 +0530

    Public scope videos can be played.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const App = () => {
   const [accessToken, setAccessToken] = useState('');
+  const [videoId, setVideoId] = useState('950642144'); // Hardcoded Vimeo video ID
 
   const generateAccessToken = async () => {
     try {
@@ -10,6 +11,7 @@ const App = () => {
       setAccessToken(response.data.access_token);
     } catch (error) {
       console.error('Error generating access token:', error);
+      // Display error to the user
     }
   };
 
@@ -18,6 +20,17 @@ const App = () => {
       <h1>Vimeo API Authentication</h1>
       <button onClick={generateAccessToken}>Generate Access Token</button>
       {accessToken && <p>Access Token: {accessToken}</p>}
+      <div>
+        {/* Replace '950642144' with the dynamic video ID */}
+        <iframe
+          src={`https://player.vimeo.com/video/${videoId}`}
+          width="640"
+          height="360"
+          frameBorder="0"
+          allowFullScreen
+          title="Vimeo Video Player"
+        ></iframe>
+      </div>
     </div>
   );
 };
